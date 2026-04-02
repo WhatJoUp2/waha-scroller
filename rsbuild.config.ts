96c464c9182783c9e17b76ed6fe4905763442cc0
WhatJoUp2 <60362991+WhatJoUp2@users.noreply.github.com>
2026-04-02 12:06:28 +0100

    prep for github pages

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,4 +4,10 @@ import { pluginReact } from "@rsbuild/plugin-react";
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  html: { title: "Wahascroller" },
+  output: {
+    // Please replace <REPO_NAME> with the repository name.
+    // For example, "/my-project/"
+    assetPrefix: "/waha-scroller/",
+  },
 });
